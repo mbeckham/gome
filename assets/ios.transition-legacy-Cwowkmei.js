0588f8c1eb640725052d37d5b75ad4ea21ba1fcb
Matthew Beckham <beckham@Matthews-MacBook-Pro.local>
2024-12-31 15:22:30 -0600

    Updates

--- assets/ios.transition-legacy-Cwowkmei.js
+++ assets/ios.transition-legacy-Cwowkmei.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Da-xqT7i.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-CKPjirnv.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
